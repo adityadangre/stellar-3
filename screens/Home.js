ee3dbbdf59c93fef3bb9522df944a66a5356a5da
adityadangre <75615989+adityadangre@users.noreply.github.com>
2021-06-24 15:01:43 +0530

    Add files via upload

---
+++ screens/Home.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Platform,
+    StatusBar,
+    ImageBackground,
+    Image
+} from "react-native";
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <ImageBackground style={styles.backgroundImage} source={require('../assets/stars.gif')}>
+                    
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
+                        this.props.navigation.navigate("Space Craft")
+                    }>
+                        <Text style={styles.routeText}>Space Crafts</Text>
+                        <Image source={require("../assets/space_crafts.png")} style={styles.iconImage}></Image>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
+                        this.props.navigation.navigate("Star Map")
+                    }>
+                        <Text style={styles.routeText}>Star Map</Text>
+                        <Image source={require("../assets/star_map.png")} style={styles.iconImage}></Image>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
+                        this.props.navigation.navigate("Daily Pictures")
+                    }>
+                        <Text style={styles.routeText}>Daily Pictures</Text>
+                        <Image source={require("../assets/daily_pictures.png")} style={styles.iconImage}></Image>
+                    </TouchableOpacity>
+
+                </ImageBackground>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        marginTop: -40
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    titleBar: {
+        flex: 0.15,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titleText: {
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "white"
+    },
+    image: {
+        height: 180,
+        width: 180,
+        marginTop: 70
+    },
+    routeCard: {
+        flex: 0.25,
+        marginLeft: 50,
+        marginRight: 50,
+        marginTop: 50,
+        borderRadius: 20,
+        backgroundColor: 'white',
+    },
+    routeText: {
+        fontSize: 35,
+        fontWeight: "bold",
+        color: "black",
+        marginTop: 75,
+        paddingLeft: 30,
+    },
+    iconImage: {
+        position: "absolute",
+        height: 80,
+        width: 80,
+        resizeMode: "contain",
+        right: 20,
+        top: 10
+    }
+})
